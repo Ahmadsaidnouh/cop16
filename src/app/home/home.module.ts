dff4efbc0679b31204d368974fa63087ee47c7f8
Ahmad Said <ahmedsaid2369@gmail.com>
2024-07-31 15:11:32 +0300

    added main/sectio54

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -7,6 +7,8 @@ import { Section1Component } from './section1/section1.component';
 import { Section2Component } from './section2/section2.component';
 import { Section3Component } from './section3/section3.component';
 import { Section4Component } from './section4/section4.component';
+import { Section5Component } from './section5/section5.component';
+import { Section6Component } from './section6/section6.component';
 
 
 @NgModule({
@@ -15,7 +17,9 @@ import { Section4Component } from './section4/section4.component';
     Section1Component,
     Section2Component,
     Section3Component,
-    Section4Component
+    Section4Component,
+    Section5Component,
+    Section6Component
   ],
   imports: [
     CommonModule,
