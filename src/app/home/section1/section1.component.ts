516bf418788f81c7f45d8d8a493c7ff7a46cb35d
Ahmad Said <ahmedsaid2369@gmail.com>
2024-07-31 03:17:21 +0300

    added home/section1

--- src/app/home/section1/section1.component.ts
+++ src/app/home/section1/section1.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./section1.component.css']
 })
 export class Section1Component {
+  toggleRegistrationList() {
+    const registrationList = document.querySelector(".registration-list") as HTMLUListElement;
+    const registrationArrow = document.querySelector(".regis-arrow") as HTMLUListElement;
 
+    registrationList.classList.toggle("display-list");
+    registrationArrow.classList.toggle("rotate-arrow");
+  }
 }
