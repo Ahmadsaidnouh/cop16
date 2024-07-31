c3589c851b55188b2a93140a2517888e57de0c6f
Ahmad Said <ahmedsaid2369@gmail.com>
2024-08-01 02:56:34 +0300

    added Explore more to main/section5

--- src/app/home/section5/section5.component.ts
+++ src/app/home/section5/section5.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./section5.component.css']
 })
 export class Section5Component {
+  toggleText(event: Event) {
+    const currentTarget = event.currentTarget as HTMLElement;
+    const previousSibling = currentTarget.previousElementSibling as HTMLElement;
+    const prevPreviousSibling = previousSibling.previousElementSibling as HTMLElement;
+
+    if (currentTarget.innerHTML === 'Explore more') {
+        currentTarget.innerHTML = 'Explore less';
+    } else if (currentTarget.innerHTML === 'Explore less') {
+        currentTarget.innerHTML = 'Explore more';
+    }
+    previousSibling.classList.toggle('d-none');
+    prevPreviousSibling.classList.toggle('d-none');
+  }
 
 }
